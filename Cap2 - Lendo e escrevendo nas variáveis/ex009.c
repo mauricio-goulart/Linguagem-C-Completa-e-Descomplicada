fda70ac4f1019286d0980b451cbfde5a25edac5e
mauricio <mauricioazevedogolf@gmail.com>
2026-01-12 15:31:13 -0300

    Ex009

--- Cap2 - Lendo e escrevendo nas variáveis/ex009.c
+++ Cap2 - Lendo e escrevendo nas variáveis/ex009.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 /*
-
-
-
+Faça um programa que leia dois valores do tipo float. Use um único comando de 
+leitura para isso. Em seguida, imprima os valores lidos na ordem inversa em que eles 
+foram lidos. 
 */
 
 int main()
 {
-    
+    float n1,n2;
+    scanf("%f %f", &n1, &n2);
+
+    printf("Valores: %f %f", n2, n1);
 }
